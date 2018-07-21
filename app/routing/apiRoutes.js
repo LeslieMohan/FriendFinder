9bbc0de09387aa7bb76bf6de5bdc5a55d1d00b06
LeslieMohan <donnalesliedesign@gmail.com>
2018-07-21 13:48:52 -0400

    current changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -19,15 +19,18 @@ module.exports = function(app) {
         for(var i = 0; i < friends.length; i++){
             //Go through list of friends in db
             total = 0;
-
+            console.log("for loop with i: " +total)
+            
             for(var j = 0; j < newScore.length; j++){
                 // calculate total value of each friend
-                var diff = parseInt(newScore[j] - friends[i].scores[j]);
+                var diff = Math.abs(newScore[j] - friends[i].scores[j]);
                 total += diff;
+                console.log("for loop with j: " +total)
             }
             if(total < bestMatch){
                 bestMatch = total;
                 index = i;
+                console.log("best match total: " +total)
             }
         }
         console.log('Best Match:', friends[index]);
